e8053f51d8ccf8220cf37a7f46a0f06463fc2c88
DusanPajic002 <dusanpajic2002@gmail.com>
2024-12-01 16:08:39 +0100

    Print bill and return respones

--- app/src/main/java/com/cyb/payten_windowsxp_terminalapp/receiver/MyReceiver.java
+++ app/src/main/java/com/cyb/payten_windowsxp_terminalapp/receiver/MyReceiver.java
@@ -7,6 +7,11 @@ import android.util.Log;
 
 import com.cyb.payten_windowsxp_terminalapp.MainActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import kotlinx.serialization.json.JsonObject;
+
 public class MyReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -19,5 +24,15 @@ public class MyReceiver extends BroadcastReceiver {
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         context.startActivity(i);
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject financialObject = jsonObject.getJSONObject("response").getJSONObject("financial");
+            JSONObject resultObject = financialObject.getJSONObject("result");
+            String message = resultObject.getString("message");
+            Log.d("MyReceiver", "Message: ${}" + message);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
