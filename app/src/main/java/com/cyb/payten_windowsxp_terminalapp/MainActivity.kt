dc3166eb0a724db2131c014adac04def3196b840
jovanSimonovic <jsimonovic7021rn@raf.rs>
2024-12-01 21:14:22 +0100

    Trying to fix navigation.

--- app/src/main/java/com/cyb/payten_windowsxp_terminalapp/MainActivity.kt
+++ app/src/main/java/com/cyb/payten_windowsxp_terminalapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cyb.payten_windowsxp_terminalapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.cyb.payten_windowsxp_terminalapp.terminalNavigation.TerminalNavigation
@@ -13,10 +14,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
-            TerminalNavigation()
+        val navigateTo = intent.getStringExtra("navigateTo")
+
+        if (navigateTo == "thank_you_screen") {
+            // Navigate to the "thank you" screen or perform related actions
+            Log.d("MainActivity", "Navigating to: $navigateTo")
+
+            setContent {
+                TerminalNavigation("thank_you_screen")
+            }
+        }else {
+            setContent {
+                TerminalNavigation("splash_screen")
+            }
         }
 
     }
+
+//    override fun onResume() {
+//        super.onResume()
+//        setContent {
+//            TerminalNavigation("thank_you_screen")
+//        }
+//    }
 
 }
